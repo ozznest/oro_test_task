27df6388a41c2dd73e75b8a39e30aea588af941b
dev.aleksey.ch <dev.aleksey.ch@alldigitalads.com>
2023-12-19 11:30:43 +0200

    fix

---
+++ src/BarBundle/AppBarBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\BarBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AppBarBundle extends Bundle
+{
+
+}
---
+++ src/BarBundle/Command/BarCommand.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\BarBundle\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class BarCommand extends Command
+{
+    public function __construct()
+    {
+        parent::__construct('bar:test');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln('bar:command');
+        return Command::SUCCESS;
+    }
+}
