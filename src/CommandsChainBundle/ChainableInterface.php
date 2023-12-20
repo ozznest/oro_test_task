329dca37d323210bca78df578e312c455ba908b5
dev.aleksey.ch <dev.aleksey.ch@alldigitalads.com>
2023-12-20 12:11:08 +0200

    refactoring add logs

--- src/CommandsChainBundle/ChainableInterface.php
+++ src/CommandsChainBundle/ChainableInterface.php
@@ -3,6 +3,7 @@
 namespace App\CommandsChainBundle;
 
 use Symfony\Component\DependencyInjection\Attribute\Autoconfigure;
+
 #[Autoconfigure(tags: ['app.chained.console'])]
 interface ChainableInterface
 {
