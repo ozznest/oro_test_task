0d3905fc89aae5462d4a2f63ff03b1a1900a8d85
dev.aleksey.ch <dev.aleksey.ch@alldigitalads.com>
2023-12-20 15:07:08 +0200

    fix styles

--- tests/CommandsChainBundle/EventSubscriber/CommandEventSubscriberTest.php
+++ tests/CommandsChainBundle/EventSubscriber/CommandEventSubscriberTest.php
@@ -10,6 +10,7 @@ use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Event\ConsoleCommandEvent;
 use Symfony\Component\Console\Event\ConsoleTerminateEvent;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -44,5 +45,24 @@ class CommandEventSubscriberTest extends TestCase
         $subscriber = new CommandEventSubscriber([$commandService], $this->loggerMock);
         $this->expectException(LogicException::class);
         $subscriber->afterCommand($consoleEvent);
+    }
+
+    public function testRunRootCommand(): void
+    {
+        $subscriber = new CommandEventSubscriber([], $this->loggerMock );
+        $command = new class('test:root') extends Command implements RootCommandInterface {
+            public function execute(InputInterface $input,OutputInterface $output) : int{
+                $output->writeln('test');
+                return Command::SUCCESS;
+            }
+        };
+        $output = $this->createMock(OutputInterface::class);
+        $output->expects(self::once())->method('write');
+
+        $subscriber->runRootCommand(new ConsoleCommandEvent(
+            $command,
+            $this->createMock(InputInterface::class),
+            $output
+        ));
     }
 }
