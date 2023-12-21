2e435d54735d73a2fc74c50d5b920680f6c42e69
dev.aleksey.ch <dev.aleksey.ch@alldigitalads.com>
2023-12-21 15:14:53 +0200

    fix services configs

--- src/CommandsChainBundle/ChainableInterface.php
+++ src/CommandsChainBundle/ChainableInterface.php
@@ -2,10 +2,13 @@
 
 namespace App\CommandsChainBundle;
 
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\DependencyInjection\Attribute\Autoconfigure;
 
 #[Autoconfigure(tags: ['app.chained.console'])]
 interface ChainableInterface
 {
     public function getRootCommand(): string;
+    public function run(InputInterface $input, OutputInterface $output): int;
 }
